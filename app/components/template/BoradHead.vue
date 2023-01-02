0e78899e89a76862bb82473ad6c2fef89c7105c8
yasuto <fujiya0101@gmail.com>
2023-01-02 22:59:12 +0900

    fix:コンポーネント分け修正

---
+++ app/components/template/BoradHead.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex justify-between w-[40%] mb-[1%] ml-[5%]">
+        <PartsBoardName>3限算数の時間</PartsBoardName>
+        <PartsMakeName>yasuto</PartsMakeName>
+        <button @click="onClick" class="text-[#D8CFCF]">
+        メモ追加
+        </button>
+    </div>
+</template>
+<script setup lang="ts">
+const emits = defineEmits<{(e: 'onChildMethodClick'): void}>()
+const onClick = (): void => {
+    emits('onChildMethodClick')
+}
+</script>
