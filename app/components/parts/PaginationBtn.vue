79d265dafcf3f1865bada4c461ec670225cc6e79
yasuto <fujiya0101@gmail.com>
2023-01-04 22:18:22 +0900

    fix:ページネーションコンポーネント分け

---
+++ app/components/parts/PaginationBtn.vue
@@ -0,0 +1,8 @@
+<template>
+    <button class="btn border-white text-black mx-3 hover:bg-[#F9FAF7]"><slot /></button>
+</template>
+<style scoped>
+    .border-white{
+        border-color: #fff !important;
+    }
+</style>
