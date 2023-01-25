2230dc97e805447412592b1bebf10dff86a99aba
yasuto <fujiya0101@gmail.com>
2023-01-25 11:15:24 +0900

    add:ボードを作成する時にバリデーションをかける

--- app/pages/makeBoard/index.vue
+++ app/pages/makeBoard/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="w-full">
-    <h1 class="text-center pb-4 text-[20px]">Hello User!!!!!</h1>
     <div class="flex items-center justify-between w-[90%] m-auto pb-11 h-[40vw]">
       <TemplateCreatedFormats>
         <template v-slot:formatName>ボードを作る</template>
